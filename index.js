2e4e1f23983ad198e09e765a3929e5875cdc1476
mimorisuzuko <hrky0206@gmail.com>
2019-03-24 14:53:23 +0900

    New

---
+++ index.js
@@ -0,0 +1,33 @@
+const { exec } = require('child_process');
+
+/**
+ * @param {number[][]|number[]} args
+ */
+const f = async (args) => {
+	const { stdout } = await new Promise((resolve, reject) => {
+		const script =
+			typeof args[0] === 'number'
+				? `c(${args.join(', ')})`
+				: args.map((a) => `c(${a.join(', ')})`).join(', ');
+
+		exec(
+			`Rscript -e "library(mvnormtest);x<-rbind(${script});mshapiro.test(x);"`,
+			(err, stdout, stderr) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve({ stdout, stderr });
+				}
+			}
+		);
+	});
+
+	const [, w, p] = stdout.match(/\s+W = (.+), p-value = (.+)\s+/);
+
+	return {
+		w: parseFloat(w),
+		p: parseFloat(p)
+	};
+};
+
+module.exports = f;
